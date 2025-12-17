f5a36563d1350d6de0e602d55f2fec8fe8386705
zawminhein <zawminheinmp@gmail.com>
2025-12-17 23:00:24 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+     <div className="min-h-screen flex items-center justify-center bg-sky-100">
+      <h1 className="text-4xl font-bold text-sky-600">
+        Tailwind is working
+      </h1>
+    </div>
+  )
+}
+
+export default App
